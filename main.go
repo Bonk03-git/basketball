affa83c58021286fec2d8604a819f4d525145d06
Bonk03-git <bartusbak@gmail.com>
2024-10-16 00:22:41 +0200

    change fyne into raylib because it is way better XD

--- main.go
+++ main.go
@@ -1,27 +1,36 @@
 package main
 
 import (
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
-	"image/color"
+	rl "github.com/gen2brain/raylib-go/raylib"
+	"math"
 )
 
 func main() {
 
-	a := app.New()
-	okno := a.NewWindow("Koszykówka")
+	rl.InitWindow(800, 600, "Koszykówka")
+	defer rl.CloseWindow()
 
-	circle := canvas.NewCircle(color.RGBA{255, 165, 0, 255})
-	circle.StrokeColor = color.RGBA{0, 0, 0, 255}
-	circle.StrokeWidth = 2
-	circle.Resize(fyne.NewSize(50, 50))
+	var radius float32 = 30
+	var posX float32 = 400
+	var posY float32 = 300
 
-	content := container.NewVBox(widget.NewLabel("Oto koło:"), circle)
+	// Główna pętla gry
+	for !rl.WindowShouldClose() {
 
-	okno.SetContent(content)
-	okno.Resize(fyne.NewSize(100, 100))
-	okno.ShowAndRun()
+		// Rysowanie
+		posX += 0.02
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.RayWhite) // Czyszczenie tła
+
+		// Pilka
+		rl.DrawCircle(int32(posX), int32(posY), radius, rl.Orange)
+		rl.DrawLine(int32(posX-radius), int32(posY), int32(posX+radius), int32(posY), rl.Black)
+		rl.DrawLine(int32(posX), int32(posY-radius), int32(posX), int32(posY+radius), rl.Black)
+		rl.DrawLine(int32(posX-radius/2), int32(float64(posY)-float64(radius)*math.Sqrt(3)/2), int32(posX-radius/2), int32(float64(posY)+float64(radius)*math.Sqrt(3)/2), rl.Black)
+		rl.DrawLine(int32(posX+radius/2), int32(float64(posY)-float64(radius)*math.Sqrt(3)/2), int32(posX+radius/2), int32(float64(posY)+float64(radius)*math.Sqrt(3)/2), rl.Black)
+		rl.EndDrawing()
+	}
 }
+
+//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
+// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
