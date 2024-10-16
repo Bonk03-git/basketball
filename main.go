c9d6816c3a9df60e5faed8b60f7eb1d945f61014
Bonk03-git <bartusbak@gmail.com>
2024-10-16 17:43:51 +0200

    rotating and moving ball

--- main.go
+++ main.go
@@ -7,27 +7,52 @@ import (
 
 func main() {
 
-	rl.InitWindow(800, 600, "Koszykówka")
+	var szerokosc int32 = 1600
+	var wysokosc int32 = 900
+
+	rl.InitWindow(szerokosc, wysokosc, "Koszykówka")
 	defer rl.CloseWindow()
 
-	var radius float32 = 30
-	var posX float32 = 400
-	var posY float32 = 300
+	var radius float64 = 30
+	var posX float64 = 200
+	var posY float64 = float64(wysokosc - 200)
+	var zmiana_kata float64 = 0
+	alpha := [5]float64{0, 60, 120, 240, 300}
 
 	// Główna pętla gry
 	for !rl.WindowShouldClose() {
 
 		// Rysowanie
 		posX += 0.02
+		zmiana_kata = 0.02
+
+		for i := 0; i < 5; i++ {
+			alpha[i] += zmiana_kata
+		}
+
+		for i := 0; i < 5; i++ {
+			if alpha[i] > 360 {
+				for j := 0; j < 1; j++ {
+					if alpha[i] > 360 {
+						alpha[i] -= 360
+						j--
+					}
+				}
+			}
+		}
+
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite) // Czyszczenie tła
 
 		// Pilka
-		rl.DrawCircle(int32(posX), int32(posY), radius, rl.Orange)
-		rl.DrawLine(int32(posX-radius), int32(posY), int32(posX+radius), int32(posY), rl.Black)
-		rl.DrawLine(int32(posX), int32(posY-radius), int32(posX), int32(posY+radius), rl.Black)
-		rl.DrawLine(int32(posX-radius/2), int32(float64(posY)-float64(radius)*math.Sqrt(3)/2), int32(posX-radius/2), int32(float64(posY)+float64(radius)*math.Sqrt(3)/2), rl.Black)
-		rl.DrawLine(int32(posX+radius/2), int32(float64(posY)-float64(radius)*math.Sqrt(3)/2), int32(posX+radius/2), int32(float64(posY)+float64(radius)*math.Sqrt(3)/2), rl.Black)
+		rl.DrawCircle(int32(posX), int32(posY), float32(radius), rl.Orange)
+
+		// radius * math.Sqrt(2-2*math.Cos(alpha[0]*math.Pi/180))
+
+		rl.DrawLine(int32(posX-radius+(radius*math.Sqrt(2-2*math.Cos(alpha[0]*math.Pi/180)))*math.Cos(((180-alpha[0])/2)*math.Pi/180)), int32(posY-(radius*math.Sqrt(2-2*math.Cos(alpha[0]*math.Pi/180)))*math.Sin(((180-alpha[0])/2)*math.Pi/180)), int32(posX+radius-(radius*math.Sqrt(2-2*math.Cos(alpha[0]*math.Pi/180)))*math.Cos(((180-alpha[0])/2)*math.Pi/180)), int32(posY+(radius*math.Sqrt(2-2*math.Cos(alpha[0]*math.Pi/180)))*math.Sin(((180-alpha[0])/2)*math.Pi/180)), rl.Black)
+		rl.DrawLine(int32(posX+(radius*math.Sqrt(2-2*math.Cos(alpha[0]*math.Pi/180)))*math.Sin(((180-alpha[0])/2)*math.Pi/180)), int32(posY-radius+(radius*math.Sqrt(2-2*math.Cos(alpha[0]*math.Pi/180)))*math.Cos(((180-alpha[0])/2)*math.Pi/180)), int32(posX-(radius*math.Sqrt(2-2*math.Cos(alpha[0]*math.Pi/180)))*math.Sin(((180-alpha[0])/2)*math.Pi/180)), int32(posY+radius-(radius*math.Sqrt(2-2*math.Cos(alpha[0]*math.Pi/180)))*math.Cos(((180-alpha[0])/2)*math.Pi/180)), rl.Black)
+		rl.DrawLine(int32(posX-radius+(radius*math.Sqrt(2-2*math.Cos(alpha[1]*math.Pi/180)))*math.Cos(((180-alpha[1])/2)*math.Pi/180)), int32(posY-(radius*math.Sqrt(2-2*math.Cos(alpha[1]*math.Pi/180)))*math.Sin(((180-alpha[1])/2)*math.Pi/180)), int32(posX-radius+(radius*math.Sqrt(2-2*math.Cos(alpha[4]*math.Pi/180)))*math.Cos(((180-alpha[4])/2)*math.Pi/180)), int32(posY-(radius*math.Sqrt(2-2*math.Cos(alpha[4]*math.Pi/180)))*math.Sin(((180-alpha[4])/2)*math.Pi/180)), rl.Black)
+		rl.DrawLine(int32(posX-radius+(radius*math.Sqrt(2-2*math.Cos(alpha[2]*math.Pi/180)))*math.Cos(((180-alpha[2])/2)*math.Pi/180)), int32(posY-(radius*math.Sqrt(2-2*math.Cos(alpha[2]*math.Pi/180)))*math.Sin(((180-alpha[2])/2)*math.Pi/180)), int32(posX-radius+(radius*math.Sqrt(2-2*math.Cos(alpha[3]*math.Pi/180)))*math.Cos(((180-alpha[3])/2)*math.Pi/180)), int32(posY-(radius*math.Sqrt(2-2*math.Cos(alpha[3]*math.Pi/180)))*math.Sin(((180-alpha[3])/2)*math.Pi/180)), rl.Black)
 		rl.EndDrawing()
 	}
 }
