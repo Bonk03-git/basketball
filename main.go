bc5f71fa9f123d4bb799cfbb03ae341cfb65d393
Bonk03-git <bartusbak@gmail.com>
2024-11-02 12:32:13 +0100

    correct moving, giving opportunity to change vector, still mistakes in rotation

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 // PAMIĘTAĆ ZAMIENIAĆ WSPOLRZEDNE X Z Y NA KONIEC PROJEKTU!!!
+// PROBLEMY
+// 1 OBROTY PIŁKI
+// 2 JAK JAKIŚ WEKTOR JEST 0 TO NIE DZIAŁA CZEMU???
 import (
+	"fmt"
 	"github.com/g3n/engine/math32"
-	rl "github.com/gen2brain/raylib-go/raylib"
-	"github.com/go-gl/mathgl/mgl32"
+	"github.com/gen2brain/raylib-go/raylib"
 	"math"
 	"math/rand"
 	"time"
@@ -50,14 +53,16 @@ func main() {
 	const szerokosc int = 1600
 	const wysokosc int = 900
 	const czas_dzialania_sily_na_pilke = 0.5
-	const krok_czasowy = 0.01
-	const g = -9.81
+	const krok_czasowy = 0.001
+	const g = -981
+
+	//100 px to 1 metr przedział
 
 	pilka := Kula{
 		promien: 0.12, // metry
 		posX:    0,    // pixele float64(rand.Intn(10))
 		posY:    0,    // pixele float64(rand.Intn(10))
-		posZ:    10,   // pixele
+		posZ:    0,    // pixele
 		rotX:    0,    // radiany
 		rotY:    0,    // radiany
 		rotZ:    0,    // radiany
@@ -68,7 +73,6 @@ func main() {
 	var wektory_sily [3]Wektor_sily
 	var sila_ruszajaca [3]float64
 	var wektory_sily_ruszajacej [3]Wektor
-	var wektory_sily_obrotowej [3]Wektor
 	var przyspieszenia_z_wektorow [3]Wektor
 	var momenty_z_wektorow [3]Wektor
 	var przyspieszenia_katowe_z_wektorow [3]Wektor
@@ -88,17 +92,17 @@ func main() {
 	for i := 0; i < 3; i++ {
 
 		punkty_przylozenia_wektorow[i] = Punkt_przylozenia{ // wyznaczane przez gracza
-			kat_fi:   math.Pi / 4,
-			kat_teta: 0,
+			kat_fi:   math.Pi,
+			kat_teta: -math.Pi/2 + math.Pi/10, // radiany
 		}
 
 		wektory_sily[i] = Wektor_sily{
-			dlugosc_x:     -0.1, // wyznacza gracz
-			dlugosc_y:     0,    // wyznacza gracz
-			dlugosc_z:     0,    // wyznacza gracz
-			przylozenie_x: pilka.promien * math.Cos(punkty_przylozenia_wektorow[i].kat_teta) * math.Cos(punkty_przylozenia_wektorow[i].kat_fi),
-			przylozenie_y: pilka.promien * math.Cos(punkty_przylozenia_wektorow[i].kat_teta) * math.Sin(punkty_przylozenia_wektorow[i].kat_fi),
-			przylozenie_z: pilka.promien * math.Sin(punkty_przylozenia_wektorow[i].kat_teta),
+			dlugosc_x:     0, // wyznacza gracz
+			dlugosc_y:     0, // wyznacza gracz
+			dlugosc_z:     0, // wyznacza gracz
+			przylozenie_x: pilka.promien * math.Cos(punkty_przylozenia_wektorow[i].kat_teta) * math.Sin(punkty_przylozenia_wektorow[i].kat_fi),
+			przylozenie_y: pilka.promien * math.Sin(punkty_przylozenia_wektorow[i].kat_teta),
+			przylozenie_z: pilka.promien * math.Cos(punkty_przylozenia_wektorow[i].kat_teta) * math.Cos(punkty_przylozenia_wektorow[i].kat_fi),
 		}
 
 		sila_ruszajaca[i] = wektory_sily[i].dlugosc_x*wektory_sily[i].przylozenie_x/pilka.promien + wektory_sily[i].dlugosc_y*wektory_sily[i].przylozenie_y/pilka.promien + wektory_sily[i].dlugosc_z*wektory_sily[i].przylozenie_z/pilka.promien
@@ -109,12 +113,6 @@ func main() {
 			z: sila_ruszajaca[i] * wektory_sily[i].przylozenie_z / pilka.promien,
 		}
 
-		wektory_sily_obrotowej[i] = Wektor{
-			x: wektory_sily[i].dlugosc_x - wektory_sily_ruszajacej[i].x,
-			y: wektory_sily[i].dlugosc_y - wektory_sily_ruszajacej[i].y,
-			z: wektory_sily[i].dlugosc_z - wektory_sily_ruszajacej[i].z,
-		}
-
 		przyspieszenia_z_wektorow[i] = Wektor{
 			x: wektory_sily_ruszajacej[i].x / pilka.masa,
 			y: wektory_sily_ruszajacej[i].y / pilka.masa,
@@ -122,9 +120,9 @@ func main() {
 		}
 
 		momenty_z_wektorow[i] = Wektor{
-			x: wektory_sily[i].przylozenie_y*wektory_sily_obrotowej[i].z - wektory_sily[i].przylozenie_z*wektory_sily_obrotowej[i].y,
-			y: -wektory_sily[i].przylozenie_x*wektory_sily_obrotowej[i].z + wektory_sily[i].przylozenie_z*wektory_sily_obrotowej[i].x,
-			z: wektory_sily[i].przylozenie_x*wektory_sily_obrotowej[i].y - wektory_sily[i].przylozenie_y*wektory_sily_obrotowej[i].x,
+			x: wektory_sily[i].przylozenie_y*wektory_sily_ruszajacej[i].z - wektory_sily[i].przylozenie_z*wektory_sily_ruszajacej[i].y,
+			y: wektory_sily[i].przylozenie_z*wektory_sily_ruszajacej[i].x - wektory_sily[i].przylozenie_x*wektory_sily_ruszajacej[i].z,
+			z: wektory_sily[i].przylozenie_x*wektory_sily_ruszajacej[i].y - wektory_sily[i].przylozenie_y*wektory_sily_ruszajacej[i].x,
 		}
 
 		przyspieszenia_katowe_z_wektorow[i] = Wektor{
@@ -185,21 +183,38 @@ func main() {
 	rl.SetMaterialTexture(&materials[0], rl.MapDiffuse, texture)
 
 	basketMesh := rl.GenMeshCube(0.1, 1.05, 1.8)
-	basketmodel := rl.LoadModelFromMesh(basketMesh)
+	basketModel := rl.LoadModelFromMesh(basketMesh)
 
-	texture_2 := rl.LoadTexture("kosz.jpg")
+	texture_2 := rl.LoadTexture("czarny.png")
 
-	materials = basketmodel.GetMaterials()
+	materials = basketModel.GetMaterials()
 	// Use SetMaterialTexture to set the texture
 	rl.SetMaterialTexture(&materials[0], rl.MapDiffuse, texture_2)
+
+	basketPosition := rl.NewVector3(5, 3, 0)
+
+	hoopMesh := rl.GenMeshTorus(0.1, 0.5, 16, 32)
+	hoopModel := rl.LoadModelFromMesh(hoopMesh)
+
+	texture_3 := rl.LoadTexture("czerwony.jpg")
+
+	materials = hoopModel.GetMaterials()
+	rl.SetMaterialTexture(&materials[0], rl.MapDiffuse, texture_3)
+
+	hoopPosition := rl.NewVector3(basketPosition.X-0.3, basketPosition.Y-0.3, basketPosition.Z)
 
 	// Set initial rotation angle
 	rotationAngle := float32(0.0)
 	rotationAxis := rl.NewVector3(1.0, 1.0, 0.0)
-	rotationAngle_2 := float32(0.0)
-	rotationAxis_2 := rl.NewVector3(0.0, 1.0, 0.0)
 
-	rl.SetTargetFPS(60) // Set our game to run at 60 frames-per-second
+	rotationAngle_2 := float32(90.0)
+	rotationAxis_2 := rl.NewVector3(1.0, 0.0, 0.0)
+
+	licznik := 0
+	licz_wartosci := true
+	faza_gry := 0
+
+	rl.SetTargetFPS(60)
 
 	// Main game loop
 	for !rl.WindowShouldClose() {
@@ -209,44 +224,150 @@ func main() {
 		if rl.IsKeyPressed(rl.KeyZ) {
 			camera.Target = rl.NewVector3(0.0, 0.0, 0.0)
 		}
+		if faza_gry == 0 {
 
-		pilka.posX = pilka.posX + predkosci_pilki.x*krok_czasowy
-		pilka.posY = pilka.posY + predkosci_pilki.y*krok_czasowy
-		pilka.posZ = pilka.posZ + predkosci_pilki.z*krok_czasowy + g*krok_czasowy*krok_czasowy/2
-		predkosci_pilki.z = predkosci_pilki.z + g*krok_czasowy
-		pilka.rotX = pilka.rotX + predkosci_katowe_pilki.x*krok_czasowy
-		pilka.rotY = pilka.rotY + predkosci_katowe_pilki.y*krok_czasowy
-		pilka.rotZ = pilka.rotZ + predkosci_katowe_pilki.z*krok_czasowy
+			if licznik == 5 {
+				faza_gry += 1
+			}
 
-		pilka.rotX = pilnowanie_zakresu_kata(pilka.rotX)
-		pilka.rotY = pilnowanie_zakresu_kata(pilka.rotY)
-		pilka.rotZ = pilnowanie_zakresu_kata(pilka.rotZ)
+			if licznik == 4 {
+				rl.DrawText(fmt.Sprintf("Przylozenie sily kat teta = %f", punkty_przylozenia_wektorow[0].kat_teta), 10, 10, 20, rl.DarkGray)
+				if rl.IsKeyPressed(rl.KeyU) {
+					punkty_przylozenia_wektorow[0].kat_teta += 2 * math32.Pi / 360
+				}
+				if rl.IsKeyPressed(rl.KeyY) {
+					punkty_przylozenia_wektorow[0].kat_teta -= 2 * math32.Pi / 360
+				}
+				if rl.IsKeyPressed(rl.KeyEnter) {
+					licznik += 1
+				}
+			}
+			if licznik == 3 {
+				rl.DrawText(fmt.Sprintf("Przylozenie sily kat fi = %f", punkty_przylozenia_wektorow[0].kat_fi), 10, 10, 20, rl.DarkGray)
+				if rl.IsKeyPressed(rl.KeyU) {
+					punkty_przylozenia_wektorow[0].kat_fi += 2 * math32.Pi / 360
+				}
+				if rl.IsKeyPressed(rl.KeyY) {
+					punkty_przylozenia_wektorow[0].kat_fi -= 2 * math32.Pi / 360
+				}
+				if rl.IsKeyPressed(rl.KeyEnter) {
+					licznik += 1
+				}
+			}
+			if licznik == 2 {
+				rl.DrawText(fmt.Sprintf("Wartosc wektora w kierunku z= %f", wektory_sily[0].dlugosc_z), 10, 10, 20, rl.DarkGray)
+				if rl.IsKeyPressed(rl.KeyU) {
+					wektory_sily[0].dlugosc_z += 5
+				}
+				if rl.IsKeyPressed(rl.KeyY) {
+					wektory_sily[0].dlugosc_z -= 5
+				}
+				if rl.IsKeyPressed(rl.KeyEnter) {
+					licznik += 1
+				}
+			}
+			if licznik == 1 {
+				rl.DrawText(fmt.Sprintf("Wartosc wektora w kierunku y= %f", wektory_sily[0].dlugosc_y), 10, 10, 20, rl.DarkGray)
+				if rl.IsKeyPressed(rl.KeyU) {
+					wektory_sily[0].dlugosc_y += 5
+				}
+				if rl.IsKeyPressed(rl.KeyY) {
+					wektory_sily[0].dlugosc_y -= 5
+				}
+				if rl.IsKeyPressed(rl.KeyEnter) {
+					licznik += 1
+				}
+			}
 
-		//kwaterniony
-
-		q_x := mgl32.QuatRotate(float32(pilka.rotX), mgl32.Vec3{1, 0, 0})
-		q_y := mgl32.QuatRotate(float32(pilka.rotY), mgl32.Vec3{0, 1, 0})
-		q_z := mgl32.QuatRotate(float32(pilka.rotZ), mgl32.Vec3{0, 0, 1})
-		q_wynik := q_z.Mul(q_y).Mul(q_x)
-
-		kat_obrotu := 2 * math.Acos(float64(q_wynik.W))
-		var os_obrotu math32.Vector3
-
-		if kat_obrotu > 0 {
-			// Normalizacja wektora osi obrotu
-			s := math.Sqrt(float64(1 - q_wynik.W*q_wynik.W))
-			os_obrotu = *math32.NewVector3(q_wynik.X()/float32(s), q_wynik.Y()/float32(s), q_wynik.Z()/float32(s))
-		} else {
-			// Oś obrotu jest nieokreślona, możemy przyjąć dowolną oś
-			os_obrotu = *math32.NewVector3(1, 0, 0) // Przykładowa oś
+			if licznik == 0 {
+				rl.DrawText(fmt.Sprintf("Wartosc wektora w kierunku x= %f", wektory_sily[0].dlugosc_x), 10, 10, 20, rl.DarkGray)
+				if rl.IsKeyPressed(rl.KeyU) {
+					wektory_sily[0].dlugosc_x += 5
+				}
+				if rl.IsKeyPressed(rl.KeyY) {
+					wektory_sily[0].dlugosc_x -= 5
+				}
+				if rl.IsKeyPressed(rl.KeyEnter) {
+					licznik += 1
+				}
+			}
 		}
 
-		rotationAxis = rl.NewVector3(os_obrotu.X, os_obrotu.Y, os_obrotu.Z)
-		rotationAngle = float32(kat_obrotu)
-		println(rotationAngle)
+		if faza_gry == 1 {
 
+			if licz_wartosci == true {
+				for i := 0; i < 3; i++ {
+
+					sila_ruszajaca[i] = wektory_sily[i].dlugosc_x*wektory_sily[i].przylozenie_x/pilka.promien + wektory_sily[i].dlugosc_y*wektory_sily[i].przylozenie_y/pilka.promien + wektory_sily[i].dlugosc_z*wektory_sily[i].przylozenie_z/pilka.promien
+
+					wektory_sily_ruszajacej[i] = Wektor{
+						x: sila_ruszajaca[i] * wektory_sily[i].przylozenie_x / pilka.promien,
+						y: sila_ruszajaca[i] * wektory_sily[i].przylozenie_y / pilka.promien,
+						z: sila_ruszajaca[i] * wektory_sily[i].przylozenie_z / pilka.promien,
+					}
+
+					przyspieszenia_z_wektorow[i] = Wektor{
+						x: wektory_sily_ruszajacej[i].x / pilka.masa,
+						y: wektory_sily_ruszajacej[i].y / pilka.masa,
+						z: wektory_sily_ruszajacej[i].z / pilka.masa,
+					}
+
+					momenty_z_wektorow[i] = Wektor{
+						x: wektory_sily[i].przylozenie_y*wektory_sily_ruszajacej[i].z - wektory_sily[i].przylozenie_z*wektory_sily_ruszajacej[i].y,
+						y: wektory_sily[i].przylozenie_z*wektory_sily_ruszajacej[i].x - wektory_sily[i].przylozenie_x*wektory_sily_ruszajacej[i].z,
+						z: wektory_sily[i].przylozenie_x*wektory_sily_ruszajacej[i].y - wektory_sily[i].przylozenie_y*wektory_sily_ruszajacej[i].x,
+					}
+
+					przyspieszenia_katowe_z_wektorow[i] = Wektor{
+						x: momenty_z_wektorow[i].x / I,
+						y: momenty_z_wektorow[i].y / I,
+						z: momenty_z_wektorow[i].z / I,
+					}
+
+					wypadkowa_przyspieszen = Wektor{
+						x: wypadkowa_przyspieszen.x + przyspieszenia_z_wektorow[i].x,
+						y: wypadkowa_przyspieszen.y + przyspieszenia_z_wektorow[i].y,
+						z: wypadkowa_przyspieszen.z + przyspieszenia_z_wektorow[i].z,
+					}
+
+					wypadkowa_przyspieszen_katowych = Wektor{
+						x: wypadkowa_przyspieszen_katowych.x + przyspieszenia_katowe_z_wektorow[i].x,
+						y: wypadkowa_przyspieszen_katowych.y + przyspieszenia_katowe_z_wektorow[i].y,
+						z: wypadkowa_przyspieszen_katowych.z + przyspieszenia_katowe_z_wektorow[i].z,
+					}
+				}
+
+				predkosci_pilki = Wektor{
+					x: wypadkowa_przyspieszen.x * czas_dzialania_sily_na_pilke,
+					y: wypadkowa_przyspieszen.y * czas_dzialania_sily_na_pilke,
+					z: wypadkowa_przyspieszen.z * czas_dzialania_sily_na_pilke,
+				}
+
+				predkosci_katowe_pilki = Wektor{
+					x: wypadkowa_przyspieszen_katowych.x * czas_dzialania_sily_na_pilke,
+					y: wypadkowa_przyspieszen_katowych.y * czas_dzialania_sily_na_pilke,
+					z: wypadkowa_przyspieszen_katowych.z * czas_dzialania_sily_na_pilke,
+				}
+				licz_wartosci = false
+			}
+			pilka.posX = pilka.posX + predkosci_pilki.x*krok_czasowy
+			pilka.posY = pilka.posY + predkosci_pilki.y*krok_czasowy + g*krok_czasowy*krok_czasowy/2
+			predkosci_pilki.y = predkosci_pilki.y + g*krok_czasowy
+			pilka.posZ = pilka.posZ + predkosci_pilki.z*krok_czasowy
+
+			pilka.rotX = pilka.rotX + predkosci_katowe_pilki.x*krok_czasowy
+			pilka.rotY = pilka.rotY + predkosci_katowe_pilki.y*krok_czasowy
+			pilka.rotZ = pilka.rotZ + predkosci_katowe_pilki.z*krok_czasowy
+
+			pilka.rotX = pilnowanie_zakresu_kata(pilka.rotX)
+			pilka.rotY = pilnowanie_zakresu_kata(pilka.rotY)
+			pilka.rotZ = pilnowanie_zakresu_kata(pilka.rotZ)
+
+		}
 		// Draw
 		rl.BeginDrawing()
+
+		println(pilka.posX, " ", pilka.posY, " ", pilka.posZ, " ", predkosci_pilki.z)
 
 		rl.ClearBackground(rl.RayWhite)
 
@@ -255,28 +376,33 @@ func main() {
 		// Draw the model with rotation
 		rl.DrawModelEx(
 			sphereModel,
-			rl.NewVector3(1.0, 0.0, 0.0), // Position
+			rl.NewVector3(float32(pilka.posX), float32(pilka.posY), float32(pilka.posZ)), // Position
 			rotationAxis,                 // Rotation axis
 			rotationAngle,                // Rotation angle
 			rl.NewVector3(1.0, 1.0, 1.0), // Scale
 			rl.White,                     // Tint
 		)
 
+		rl.DrawModel(
+			basketModel,
+			basketPosition, // Position
+			1,
+			rl.White, // Tint
+		)
+
 		rl.DrawModelEx(
-			basketmodel,
-			rl.NewVector3(10.0, 0.0, 0.0), // Position
-			rotationAxis_2,                // Rotation axis
-			rotationAngle_2,               // Rotation angle
-			rl.NewVector3(1.0, 1.0, 1.0),  // Scale
-			rl.White,                      // Tint
+			hoopModel,
+			hoopPosition,
+			rotationAxis_2,
+			rotationAngle_2,
+			rl.NewVector3(1.0, 1.0, 1.0),
+			rl.White,
 		)
 
 		// Optionally, draw a grid for reference
 		rl.DrawGrid(100, 1.0)
 
 		rl.EndMode3D()
-
-		rl.DrawText("Rotating Basketball", 10, 10, 20, rl.DarkGray)
 
 		rl.EndDrawing()
 	}
